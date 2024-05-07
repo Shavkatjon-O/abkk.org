2977752376e3d70e3c95c9b23f375cf14344e8f9
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-07 14:22:20 +0500

    feat: boilerplate

---
+++ core/settings/base.py
@@ -0,0 +1,109 @@
+import environ
+from pathlib import Path
+
+env = environ.Env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = "zi6e#u)51eu)2@)jhz0!suj6us0_j(z!qv(-n)so(@7#3%rys#"
+
+DJANGO_APPS = [
+    "jazzmin",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+CUSTOM_APPS = [
+    "common",
+    "theme",
+]
+
+THIRD_PARTY_APPS = [
+    "tailwind",
+    "rosetta",
+    "django_browser_reload",
+]
+
+INSTALLED_APPS = DJANGO_APPS + CUSTOM_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # other middlewares
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    "core.middleware.ErrorHandlerMiddleware",
+]
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "core.wsgi.application"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TIME_ZONE = "Asia/Tashkent"
+
+USE_L10N = True
+USE_I18N = True
+USE_TZ = True
+
+LANGUAGE_CODE = "en"
+LANGUAGES = (
+    ("ru", "Russian"),
+    ("uz", "Uzbek"),
+    ("en", "English"),
+)
+LOCALE_PATHS = (BASE_DIR / "locale/",)
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+NPM_BIN_PATH = env.str("NPM_BIN_PATH")
---
+++ core/settings/production.py
@@ -0,0 +1,22 @@
+from core.settings.base import *
+
+ALLOWED_HOSTS = [
+    "tempo-clone.dev-space.uz",
+]
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASS"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT"),
+    }
+}
+
+# Telegram Error Log Settings
+TELEGRAM_CHAT_ID = env.str("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = env.str("TELEGRAM_BOT_TOKEN")
