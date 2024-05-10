4b8565900672ad8ecd06b95bc4af488f14c7959d
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-10 14:52:49 +0500

    fix: reduce the numebr of workers to 3

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,12 +1,9 @@
 import environ
-import multiprocessing
 
 env = environ.Env()
 env.read_env(".env")
 
-cpu_count = multiprocessing.cpu_count()
-
 DJANGO_WEB_PORT = env.str("DJANGO_WEB_PORT")
 
 bind = f"0.0.0.0:{DJANGO_WEB_PORT}"
-workers = cpu_count * 2 + 1
+workers = 3
