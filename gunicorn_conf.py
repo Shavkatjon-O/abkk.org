91be1fe7a4080cc0475745fe1f07c63321fdfa17
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-10 14:51:12 +0500

    fix: gunicorn conf

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,10 +1,12 @@
 import environ
 import multiprocessing
 
-env = environ.Env().read_env(".env")
+env = environ.Env()
+env.read_env(".env")
+
 cpu_count = multiprocessing.cpu_count()
 
-DJANGO_WEB_PORT = env.int("DJANGO_WEB_PORT")
+DJANGO_WEB_PORT = env.str("DJANGO_WEB_PORT")
 
 bind = f"0.0.0.0:{DJANGO_WEB_PORT}"
 workers = cpu_count * 2 + 1
