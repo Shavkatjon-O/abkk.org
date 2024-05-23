29f3f62bee7850d504a2047d800f8cdd26b77924
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-23 23:59:44 +0500

    feat: model translation

--- about/admin.py
+++ about/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import AboutConfederation
 
-admin.site.register(AboutConfederation)
+
+@admin.register(AboutConfederation)
+class AboutConfederationAdmin(admin.ModelAdmin):
+    exclude = ("content",)
