d2f3ecbf9f3d7546f2854b653d70a6240f52363e
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-23 23:22:36 +0500

    fix: remove test model

--- about/admin.py
+++ about/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import TestModel
-
-admin.site.register(TestModel)
--- about/models.py
+++ about/models.py
@@ -1,6 +1,2 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-
-
-class TestModel(models.Model):
-    text = RichTextField()
