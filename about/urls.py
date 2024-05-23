b3ecf5c368e2d2b2b0d320b8c6caa4ca410817e8
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-23 14:03:04 +0500

    feat: links

--- about/urls.py
+++ about/urls.py
@@ -3,10 +3,14 @@ from about import views
 
 
 urlpatterns = [
-    path("confederation/", views.ConfederationTemplateView.as_view()),
-    path("guides/", views.GuidesTemplateView.as_view()),
-    path("documents/", views.DocumentsTemplateView.as_view()),
-    path("regulations/", views.RegulationsTemplateView.as_view()),
-    path("symbols/", views.SymbolsTemplateView.as_view()),
-    path("membership/", views.MembershipView.as_view()),
+    path(
+        "confederation/",
+        views.ConfederationTemplateView.as_view(),
+        name="confederation",
+    ),
+    path("guides/", views.GuidesTemplateView.as_view(), name="guides"),
+    path("documents/", views.DocumentsTemplateView.as_view(), name="documents"),
+    path("regulations/", views.RegulationsTemplateView.as_view(), name="regulations"),
+    path("symbols/", views.SymbolsTemplateView.as_view(), name="symbols"),
+    path("membership/", views.MembershipView.as_view(), name="membership"),
 ]
