62ae271c4ffc56a57bf46a982de2e157a66134e0
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-23 22:44:28 +0500

    fix: urls

--- core/urls.py
+++ core/urls.py
@@ -3,22 +3,17 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
-def index(request):
-    from django.shortcuts import render
-
-    return render(request, "home.html")
+from common.views import home_page_view
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rosetta/", include("rosetta.urls")),
-    path("", index),
+    path("", home_page_view),
     path("about/", include("about.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
-    urlpatterns += [path("ckeditor5/", include("django_ckeditor_5.urls"))]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
