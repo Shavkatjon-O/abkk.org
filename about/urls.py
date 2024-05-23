ab92f28923a5cf19904f4cd9f8fdad6a04e8f260
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-23 22:40:02 +0500

    fix: urls

--- about/urls.py
+++ about/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
-from about import views
-
+from . import views
 
 urlpatterns = [
-    path(
-        "confederation/",
-        views.ConfederationTemplateView.as_view(),
-        name="confederation",
-    ),
-    path("guides/", views.GuidesTemplateView.as_view(), name="guides"),
-    path("documents/", views.DocumentsTemplateView.as_view(), name="documents"),
-    path("regulations/", views.RegulationsTemplateView.as_view(), name="regulations"),
-    path("symbols/", views.SymbolsTemplateView.as_view(), name="symbols"),
+    path("confederation/", views.ConfederationView.as_view(), name="confederation"),
+    path("guides/", views.GuidesView.as_view(), name="guides"),
+    path("documents/", views.DocumentsView.as_view(), name="documents"),
+    path("regulations/", views.RegulationsView.as_view(), name="regulations"),
+    path("symbols/", views.SymbolsView.as_view(), name="symbols"),
     path("membership/", views.MembershipView.as_view(), name="membership"),
 ]
--- about/views.py
+++ about/views.py
@@ -2,28 +2,23 @@ from django.views.generic import TemplateView
 from about import models
 
 
-class ConfederationTemplateView(TemplateView):
+class ConfederationView(TemplateView):
     template_name = "about/confederation.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["common_objects"] = models.Common.objects.first()
-        return context
 
-
-class GuidesTemplateView(TemplateView):
+class GuidesView(TemplateView):
     template_name = "about/guides.html"
 
 
-class DocumentsTemplateView(TemplateView):
+class DocumentsView(TemplateView):
     template_name = "about/documents.html"
 
 
-class RegulationsTemplateView(TemplateView):
+class RegulationsView(TemplateView):
     template_name = "about/regulations.html"
 
 
-class SymbolsTemplateView(TemplateView):
+class SymbolsView(TemplateView):
     template_name = "about/symbols.html"
 
 
