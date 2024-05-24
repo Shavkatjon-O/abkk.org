419a71d2431ecb4a77f1e781131cbe166550ff33
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-24 22:50:05 +0500

    feat: confedration data

--- about/views.py
+++ about/views.py
@@ -1,18 +1,18 @@
 from django.views.generic import TemplateView
-from .models import AboutConfederation
+from . import models
 
 
 class ConfederationView(TemplateView):
     template_name = "about/confederation.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["confederation"] = models.AboutConfederation.objects.first()
+        return context
+
 
 class GuidesView(TemplateView):
     template_name = "about/guides.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["confederation"] = AboutConfederation.objects.first()
-        return context
 
 
 class DocumentsView(TemplateView):
