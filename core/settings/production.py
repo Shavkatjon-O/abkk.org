f6d6971de36f23a2211a24d3afe0b20f40b1658b
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-25 23:21:09 +0500

    fix: allowed hosts

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,6 +1,6 @@
 from core.settings.base import *
 
-ALLOWED_HOSTS = ["abkk-org.dev-space.uz"]
+ALLOWED_HOSTS = ["abkk.org"]
 
 DEBUG = False
 
@@ -16,7 +16,7 @@ DATABASES = {
 }
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ["https://abkk-org.dev-space.uz"]
+CSRF_TRUSTED_ORIGINS = ["https://abkk.org"]
 
 TELEGRAM_CHAT_ID = env.str("TELEGRAM_CHAT_ID")
 TELEGRAM_BOT_TOKEN = env.str("TELEGRAM_BOT_TOKEN")
