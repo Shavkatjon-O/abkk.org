43a3c90352fa6f84f950c097fd0053968da1ad16
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-27 13:31:33 +0500

    fix: views

--- common/views.py
+++ common/views.py
@@ -1,62 +1,61 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
 from . import models
 
 
-def home_page(request):
-    return render(request, "home.html")
+class HomePageView(TemplateView):
+    template_name = "home.html"
 
 
 class ConfederationView(TemplateView):
-    template_name = "about/confederation.html"
+    template_name = "confederation.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["confederation"] = models.AboutConfederation.objects.first()
+        context["confederation"] = models.Confederation.objects.first()
         return context
 
 
 class GuidesView(TemplateView):
-    template_name = "about/guides.html"
+    template_name = "guides.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["guides"] = models.AboutGuides.objects.first()
+        context["guides"] = models.Guides.objects.first()
         return context
 
 
 class DocumentsView(TemplateView):
-    template_name = "about/documents.html"
+    template_name = "documents.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["documents"] = models.AboutDocuments.objects.first()
+        context["documents"] = models.Documents.objects.first()
         return context
 
 
 class RegulationsView(TemplateView):
-    template_name = "about/regulations.html"
+    template_name = "regulations.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["regulations"] = models.AboutRegulations.objects.first()
+        context["regulations"] = models.Regulations.objects.first()
         return context
 
 
 class SymbolsView(TemplateView):
-    template_name = "about/symbols.html"
+    template_name = "symbols.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["symbols"] = models.AboutSymbols.objects.first()
+        context["symbols"] = models.Symbols.objects.first()
         return context
 
 
 class MembershipView(TemplateView):
-    template_name = "about/membership.html"
+    template_name = "membership.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["membership"] = models.AboutMembership.objects.first()
+        context["membership"] = models.Membership.objects.first()
         return context
