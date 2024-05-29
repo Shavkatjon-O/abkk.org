a3aa3f30b6aac79ec6372e020d25bc2b6a9abb1d
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-29 09:56:05 +0500

    fix: admin

--- common/models.py
+++ common/models.py
@@ -64,8 +64,8 @@ class Documents(BaseModel):
     document = models.FileField(upload_to="documents/", null=True, blank=True)
 
     class Meta:
-        verbose_name = "Documents"
-        verbose_name_plural = "Documents"
+        verbose_name = "Учредительные документы"
+        verbose_name_plural = "Учредительные документы"
 
     def __str__(self):
         return self.title or str(self.id)
@@ -76,8 +76,8 @@ class Regulations(BaseModel):
     document = models.FileField(upload_to="documents/", null=True, blank=True)
 
     class Meta:
-        verbose_name = "Regulations"
-        verbose_name_plural = "Regulations"
+        verbose_name = "Устав"
+        verbose_name_plural = "Устав"
 
     def __str__(self):
         return self.title or str(self.id)
