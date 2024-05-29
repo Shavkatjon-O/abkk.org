d5778f97483650aa0274d54896c4559d0d3834be
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-29 09:52:45 +0500

    fix: admins

--- common/admin.py
+++ common/admin.py
@@ -3,24 +3,24 @@ from common.mixins import TabbedTranslationAdmin
 from common import models
 
 
-@admin.register(models.Confederation)
-class ConfederationAdmin(TabbedTranslationAdmin):
-    pass
+# @admin.register(models.Confederation)
+# class ConfederationAdmin(TabbedTranslationAdmin):
+#     pass
 
 
-@admin.register(models.Guides)
-class GuidesAdmin(TabbedTranslationAdmin):
-    pass
+# @admin.register(models.Guides)
+# class GuidesAdmin(TabbedTranslationAdmin):
+#     pass
 
 
-@admin.register(models.Membership)
-class MembershipAdmin(TabbedTranslationAdmin):
-    pass
+# @admin.register(models.Membership)
+# class MembershipAdmin(TabbedTranslationAdmin):
+#     pass
 
 
-@admin.register(models.Symbols)
-class SymbolsAdmin(TabbedTranslationAdmin):
-    pass
+# @admin.register(models.Symbols)
+# class SymbolsAdmin(TabbedTranslationAdmin):
+#     pass
 
 
 @admin.register(models.Documents)
@@ -33,11 +33,11 @@ class RegulationsAdmin(TabbedTranslationAdmin):
     pass
 
 
-@admin.register(models.History)
-class HistoryAdmin(TabbedTranslationAdmin):
-    pass
+# @admin.register(models.History)
+# class HistoryAdmin(TabbedTranslationAdmin):
+#     pass
 
 
-@admin.register(models.Rules)
-class RulesAdmin(TabbedTranslationAdmin):
-    pass
+# @admin.register(models.Rules)
+# class RulesAdmin(TabbedTranslationAdmin):
+#     pass
