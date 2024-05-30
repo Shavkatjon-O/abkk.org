a78a24be73da0bc25cdfea9265a0ea4c569c9f7b
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-30 17:09:23 +0500

    fix

--- core/urls.py
+++ core/urls.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf.urls.i18n import set_language
+from django.utils.translation import gettext_lazy as _
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("set_language/", set_language, name="set_language"),
     path("rosetta/", include("rosetta.urls")),
-    path("", include("common.urls")),
 ]
+
+urlpatterns += i18n_patterns(
+    path("", include("common.urls")),
+)
 
 if settings.DEBUG:
     urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
