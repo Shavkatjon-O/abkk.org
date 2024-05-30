d229621a5c1344ff050ef29a12246811e958aa38
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-30 16:30:38 +0500

    fix

--- common/admin.py
+++ common/admin.py
@@ -27,31 +27,31 @@ admin.site.register(models.CarouselImage)
 admin.site.register(models.GalleryImage)
 
 
-# @admin.register(models.Confederation)
-# class ConfederationAdmin(TabbedTranslationAdmin):
-#     pass
+@admin.register(models.Confederation)
+class ConfederationAdmin(TabbedTranslationAdmin):
+    pass
 
 
-# @admin.register(models.Guides)
-# class GuidesAdmin(TabbedTranslationAdmin):
-#     pass
+@admin.register(models.Guides)
+class GuidesAdmin(TabbedTranslationAdmin):
+    pass
 
 
-# @admin.register(models.Membership)
-# class MembershipAdmin(TabbedTranslationAdmin):
-#     pass
+@admin.register(models.Membership)
+class MembershipAdmin(TabbedTranslationAdmin):
+    pass
 
 
-# @admin.register(models.Symbols)
-# class SymbolsAdmin(TabbedTranslationAdmin):
-#     pass
+@admin.register(models.Symbols)
+class SymbolsAdmin(TabbedTranslationAdmin):
+    pass
 
 
-# @admin.register(models.History)
-# class HistoryAdmin(TabbedTranslationAdmin):
-#     pass
+@admin.register(models.History)
+class HistoryAdmin(TabbedTranslationAdmin):
+    pass
 
 
-# @admin.register(models.Rules)
-# class RulesAdmin(TabbedTranslationAdmin):
-#     pass
+@admin.register(models.Rules)
+class RulesAdmin(TabbedTranslationAdmin):
+    pass
