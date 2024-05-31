780e0aad1581cc7c416e1db97e3d27f1fde82f40
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-06-01 02:30:11 +0500

    fix

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -6,13 +6,13 @@ env.read_env(".env")
 
 DJANGO_WEB_PORT = env.str("DJANGO_WEB_PORT")
 
-bind = f"0.0.0.0:{DJANGO_WEB_PORT}"
-workers = 3
-
 log_dir = "logs"
 
 if not os.path.exists(log_dir):
     os.mkdir(log_dir)
 
-log_access = os.path.join(log_dir, "access.log")
-log_error = os.path.join(log_dir, "error.log")
+accesslog = os.path.join(log_dir, "access.log")
+errorlog = os.path.join(log_dir, "error.log")
+
+bind = f"0.0.0.0:{DJANGO_WEB_PORT}"
+workers = 3
