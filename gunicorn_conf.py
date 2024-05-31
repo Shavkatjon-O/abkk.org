c35bb7b2cf6b221dc2da7fcbe4441e898e7ed5bb
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-06-01 02:25:40 +0500

    feat: logs

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,3 +1,4 @@
+import os
 import environ
 
 env = environ.Env()
@@ -7,3 +8,11 @@ DJANGO_WEB_PORT = env.str("DJANGO_WEB_PORT")
 
 bind = f"0.0.0.0:{DJANGO_WEB_PORT}"
 workers = 3
+
+log_dir = "logs"
+
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir)
+
+log_access = os.path.join(log_dir, "access.log")
+log_error = os.path.join(log_dir, "error.log")
