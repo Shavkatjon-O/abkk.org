9aef68e290ff8916079d89d53b00b8c9c407dc04
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-05-31 09:34:12 +0500

    fix

--- common/views.py
+++ common/views.py
@@ -8,7 +8,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["carousel"] = models.CarouselImage.objects.all()
+        context["carousel"] = models.Carousel.objects.all()
         context["gallery"] = models.Gallery.objects.all()
         return context
 
