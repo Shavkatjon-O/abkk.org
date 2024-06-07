3361fbfd0cc24c59cf44f355b021b6d13be684c3
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-06-07 17:19:48 +0500

    feat: about models

--- apps/about/models.py
+++ apps/about/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from ckeditor_uploader.fields import RichTextUploadingField
+from apps.common.models import BaseModel
+
+
+class Confederation(BaseModel):
+    content = RichTextUploadingField()
+
+    class Meta:
+        verbose_name = _("confederation")
+        verbose_name_plural = _("confederation")
+
+
+class Leadership(BaseModel):
+    content = RichTextUploadingField()
+
+    class Meta:
+        verbose_name = _("leadership")
+        verbose_name_plural = _("leadership")
+
+
+class ConstituentDocuments(BaseModel):
+    title = models.CharField(max_length=256)
+    document = models.FileField(upload_to="documents/")
+
+    class Meta:
+        verbose_name = _("constituent documents")
+        verbose_name_plural = _("constituent documents")
+
+
+class Charter(BaseModel):
+    title = models.CharField(max_length=256)
+    document = models.FileField(upload_to="documents/")
+
+    class Meta:
+        verbose_name = _("charter")
+        verbose_name_plural = _("charter")
+
+
+class Membership(BaseModel):
+    content = RichTextUploadingField()
+
+    class Meta:
+        verbose_name = _("membership")
+        verbose_name_plural = _("membership")
